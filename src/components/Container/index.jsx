e2af5c2add7c26ca981ee1bd6c83629d0f0e0bce
Катерина Гривнак <hryvnak@flarian.ru>
2022-09-07 17:29:58 +0300

    change Container component

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,8 +1,13 @@
+import PropTypes from 'prop-types';
 import { StyledContainer } from "./index.styles";
 
 const Container = (props) => {
   const { children } = props;
   return <StyledContainer>{ children }</StyledContainer>
+}
+
+Container.propTypes = {
+  children: PropTypes.node,
 }
 
 export default Container;
