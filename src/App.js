f2e5d7e89f98f1ce0bfe4075f694cf8ef3c92b1a
Катерина Гривнак <hryvnak@flarian.ru>
2022-09-13 21:56:22 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content={theme.palette.primary.main} />
         </Helmet>
         <AnimatePresence mode="wait">
           <Routes location={location} key={location.pathname}>
